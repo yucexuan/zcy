6e334c2c1b82ccfb36bde2b5cd3df1748e0b20a9
yucexuan <a1228769451@163.com>
2020-11-29 13:38:30 +0800

    项目初始化

---
+++ src/test/java/com/ycx/zcy/ZcyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycx.zcy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZcyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
